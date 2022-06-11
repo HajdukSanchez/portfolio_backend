af9c751dfedea9c5e34517b84681be2cc26ade35
hajduksanchez <hajduksanchez.dev@gmail.com>
2022-06-11 11:07:24 -0500

    Update database config

---
+++ src/api/certificates-page/controllers/certificates-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  certificates-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::certificates-page.certificates-page');
---
+++ src/api/certificates-page/services/certificates-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * certificates-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::certificates-page.certificates-page');
